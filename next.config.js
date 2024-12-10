a253dd972a2504662a64af967bbd5551e2a0fa39
yepvaishz <poti.vaishnavi21@gmail.com>
2024-12-11 00:45:46 +0530

    4.4

--- next.config.js
+++ next.config.js
@@ -1,9 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   webpack: (config) => {
     config.resolve.alias.canvas = false
     config.resolve.alias.encoding = false
     return config
+  },
+  env: {
+    NEXT_PUBLIC_AWS_REGION: process.env.NEXT_PUBLIC_AWS_REGION,
+    NEXT_PUBLIC_AWS_ACCESS_KEY_ID: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+    NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+  },
+  // Add this to handle API routes in production
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ]
   }
 }
 
